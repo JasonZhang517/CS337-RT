910a563773d288a4d3a7579eb581a0ec6821d7a4
qdelta <qdelta@sjtu.edu.cn>
2021-10-31 13:13:25 +0800

    initial commit

---
+++ XUSG.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "stdafx.h"
+
+#include <Core/XUSG.h>
+#include <Advanced/XUSGAdvanced.h>
+#include <RayTracing/XUSGRayTracing.h>
+#include <Ultimate/XUSGUltimate.h>
+
---
+++ stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <map>
+
+#include <DirectXMath.h>
+#include <d3d12.h>
+#include <winrt/base.h>
