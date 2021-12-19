d6b5f7b6dbbd6d3841a75f8c4d8dff305f98c0f9
JasonZhang517 <yixin2001@163.com>
2021-12-19 10:10:30 +0800

    hull&domain shader

--- RT-Granularity/Content/Shaders/DomainShader.hlsl
+++ RT-Granularity/Content/Shaders/DomainShader.hlsl
@@ -1,59 +1,48 @@
-/*
 struct DS_OUTPUT
 {
-	float4 vPosition  : SV_POSITION;
+	float3 vPosition : WORLDPOS;
+	float3 vNormal  : NORMAL;
 };
 
 struct HS_CONTROL_POINT_OUTPUT
 {
-	float3 vPosition : WORLDPOS; 
+	float3 vPosition : WORLDPOS;
+	float3 vNormal  : NORMAL;
 };
 
 struct HS_CONSTANT_DATA_OUTPUT
 {
-	float EdgeTessFactor[3]			: SV_TessFactor;
-	float InsideTessFactor			: SV_InsideTessFactor;
-};
-*/
-struct DSOut
-{
-	float4	Pos		: SV_POSITION;
-	float4	CSPos	: POSCURRENT;
-	float4	TSPos 	: POSHISTORY;
-	float3	Norm	: NORMAL;
-	float2	UV		: TEXCOORD;
-};
-
-struct HSOut
-{
-	float4	Pos		: POSITION;
-	float4	CSPos	: POSCURRENT;
-	float4	TSPos 	: POSHISTORY;
-	float3	Norm	: NORMAL;
-	float2	UV		: TEXCOORD;
-};
-
-struct HSConstOut
-{
-	float EdgeTessFactor[3]	: SV_TessFactor;
+	float EdgeTessFactor[3]   : SV_TessFactor;
 	float InsideTessFactor : SV_InsideTessFactor;
 };
 
-#define NUM_CONTROL_POINTS 3
+#define NUM_CONTROL_POINTS 13
 
 [domain("tri")]
-DSOut main(
-	HSConstOut input,
+DS_OUTPUT main(
+	HS_CONSTANT_DATA_OUTPUT input,
 	float3 domain : SV_DomainLocation,
-	const OutputPatch<HSOut, NUM_CONTROL_POINTS> patch)
+	const OutputPatch<HS_CONTROL_POINT_OUTPUT, NUM_CONTROL_POINTS> patch)
 {
-	DSOut Output;
+	DS_OUTPUT Output;
 
-	Output.Pos = patch[0].Pos * domain.x + patch[1].Pos * domain.y + patch[2].Pos * domain.z;
-	Output.CSPos = patch[0].CSPos * domain.x + patch[1].CSPos * domain.y + patch[2].CSPos * domain.z;
-	Output.TSPos = patch[0].TSPos * domain.x + patch[1].TSPos * domain.y + patch[2].TSPos * domain.z;
-	Output.Norm = patch[0].Norm * domain.x + patch[1].Norm * domain.y + patch[2].Norm * domain.z;
-	Output.UV = patch[0].UV * domain.x + patch[1].UV * domain.y + patch[2].UV * domain.z;
+	float u = domain.x;
+	float v = domain.y;
+	float w = domain.z;
+
+	float3 vWorldPos = (patch[0].vPosition * pow(w, 3)) + (patch[1].vPosition * pow(u, 3)) + (patch[2].vPosition * pow(v, 3))
+		+ (patch[3].vPosition * 3.0f * pow(w, 2) * u)
+		+ (patch[4].vPosition * 3.0f * w * pow(u, 2))
+		+ (patch[8].vPosition * 3.0f * pow(w, 2) * v)
+		+ (patch[5].vPosition * 3.0f * pow(u, 2) * v)
+		+ (patch[7].vPosition * 3.0f * w * pow(v, 2))
+		+ (patch[6].vPosition * 3.0f * u * pow(v, 2))
+		+ (patch[9].vPosition * 6.0f * w * u * v);
+	Output.vPosition = vWorldPos;
+
+	float3 vWorldNorm = (pow(w, 2) * patch[0].vNormal) + (pow(u, 2) * patch[1].vNormal) + (pow(v, 2) * patch[2].vNormal)
+		+ (w * u * patch[10].vNormal) + (u * v * patch[11].vNormal) + (w * v * patch[12].vNormal);
+	Output.vNormal = normalize(vWorldNorm);
 
 	return Output;
 }
--- RT-Granularity/Content/Shaders/HullShader.hlsl
+++ RT-Granularity/Content/Shaders/HullShader.hlsl
@@ -1,72 +1,133 @@
-struct VSOut
-{
-	float4	Pos		: POSITION;
-	float4	CSPos	: POSCURRENT;
-	float4	TSPos 	: POSHISTORY;
-	float3	Norm	: NORMAL;
-	float2	UV		: TEXCOORD;
-};
-
-struct HSOut
-{
-	float4	Pos		: POSITION;
-	float4	CSPos	: POSCURRENT;
-	float4	TSPos 	: POSHISTORY;
-	float3	Norm	: NORMAL;
-	float2	UV		: TEXCOORD;
-};
-
-/*
 struct VS_CONTROL_POINT_OUTPUT
 {
 	float3 vPosition : WORLDPOS;
+	float3 vNormal : NORMAL;
 };
 
 struct HS_CONTROL_POINT_OUTPUT
 {
 	float3 vPosition : WORLDPOS;
+	float3 vNormal : NORMAL;
 };
-*/
 
-struct HSConstOut
+struct HS_CONSTANT_DATA_OUTPUT
 {
-	float EdgeTessFactor[3]	: SV_TessFactor;
-	float InsideTessFactor : SV_InsideTessFactor;
+	float EdgeTessFactor[3]			: SV_TessFactor;
+	float InsideTessFactor			: SV_InsideTessFactor;
 };
 
-#define NUM_CONTROL_POINTS 3
+float ComputeWeight(InputPatch<VS_CONTROL_POINT_OUTPUT, 3> inPatch, int i, int j)
+{
+	return dot(inPatch[j].vPosition - inPatch[i].vPosition, inPatch[i].vNormal);
+}
 
-HSConstOut CalcHSPatchConstants(
-	InputPatch<VSOut, NUM_CONTROL_POINTS> ip,
+float3 ComputeEdgePosition(InputPatch<VS_CONTROL_POINT_OUTPUT, 3> inPatch, int i, int j)
+{
+	return (
+		(2.0f * inPatch[i].vPosition) + inPatch[j].vPosition
+		- (ComputeWeight(inPatch, i, j) * inPatch[i].vNormal)
+		) / 3.0f;
+}
+
+float3 ComputeEdgeNormal(InputPatch<VS_CONTROL_POINT_OUTPUT, 3> inPatch, int i, int j)
+{
+	float t = dot(inPatch[j].vPosition - inPatch[i].vPosition,
+		inPatch[i].vNormal + inPatch[j].vNormal);
+	float b = dot(inPatch[j].vPosition - inPatch[i].vPosition,
+		inPatch[j].vPosition - inPatch[i].vPosition);
+	float v = 2.0f * (t / b);
+	return normalize(
+		inPatch[i].vNormal + inPatch[j].vNormal
+		-v*(inPatch[j].vPosition-inPatch[i].vPosition)
+	);
+}
+
+#define NUM_CONTROL_POINTS 13
+
+HS_CONSTANT_DATA_OUTPUT CalcHSPatchConstants(
+	InputPatch<VS_CONTROL_POINT_OUTPUT, 3> ip,
 	uint PatchID : SV_PrimitiveID)
 {
-	HSConstOut Output;
+	HS_CONSTANT_DATA_OUTPUT Output;
 
-	Output.EdgeTessFactor[0] = 
-		Output.EdgeTessFactor[1] = 
-		Output.EdgeTessFactor[2] = 
-		Output.InsideTessFactor = 3;
+	Output.EdgeTessFactor[0] = 64.0f;
+	Output.EdgeTessFactor[1] = 64.0f;
+		Output.EdgeTessFactor[2] = 64.0f;
+
+	Output.InsideTessFactor = 64.0f;
 
 	return Output;
 }
 
 [domain("tri")]
-[partitioning("integer")]
+[partitioning("fractional_even")]
 [outputtopology("triangle_cw")]
 [outputcontrolpoints(NUM_CONTROL_POINTS)]
 [patchconstantfunc("CalcHSPatchConstants")]
-HSOut main(
-	InputPatch<VSOut, NUM_CONTROL_POINTS> ip,
+HS_CONTROL_POINT_OUTPUT main( 
+	InputPatch<VS_CONTROL_POINT_OUTPUT, 3> ip, 
 	uint i : SV_OutputControlPointID,
 	uint PatchID : SV_PrimitiveID )
 {
-	HSOut Output;
+	HS_CONTROL_POINT_OUTPUT Output;
 
-	Output.Pos = ip[i].Pos;
-	Output.CSPos = ip[i].CSPos;
-	Output.TSPos = ip[i].TSPos;
-	Output.Norm = ip[i].Norm;
-	Output.UV = ip[i].UV;
+	Output.vPosition = float3(0.0f,0.0f,0.0f);
+	Output.vNormal = float3(0.0f, 0.0f, 0.0f);
 
+	switch (i)
+	{
+	case 0:
+
+	case 1:
+
+	case 2:
+		Output.vPosition = ip[i].vPosition;
+		Output.vNormal = ip[i].vNormal;
+		break;
+	//edge between v0 and v1
+	//b(210)
+	case 3:
+		Output.vPosition = ComputeEdgePosition(ip, 0, 1);
+		break;
+	//b(120)
+	case 4:
+		Output.vPosition = ComputeEdgePosition(ip, 1, 0);
+		break;
+	//edge between v1 and v2
+	//b(021)
+	case 5:
+		Output.vPosition = ComputeEdgePosition(ip, 1, 2);
+		break;
+	//b(012)
+	case 6:
+		Output.vPosition = ComputeEdgePosition(ip, 2, 1);
+		break;
+	//edge between v2 and v0
+	//b(102)
+	case 7:
+		Output.vPosition = ComputeEdgePosition(ip, 2, 0);
+		break;
+	//b(201)
+	case 8:
+		Output.vPosition = ComputeEdgePosition(ip, 0, 2);
+		break;
+	//b(111)
+	case 9:
+		float3 E = (ComputeEdgePosition(ip, 0, 1) + ComputeEdgePosition(ip, 1, 0) + ComputeEdgePosition(ip, 2, 0) + ComputeEdgePosition(ip, 0, 2)
+			+ ComputeEdgePosition(ip, 2, 1) + ComputeEdgePosition(ip, 1, 2)) / 6.0f;
+		float3 V = (ip[0].vPosition+ ip[1].vPosition+ ip[2].vPosition) / 3.0f;
+		Output.vPosition = E + ((E - V) / 2.0f);
+		break;
+	//n(110)
+	case 10:
+		Output.vNormal = ComputeEdgeNormal(ip, 0, 1);
+		break;
+	case 11:
+		Output.vNormal = ComputeEdgeNormal(ip, 1, 2);
+		break;
+	case 12:
+		Output.vNormal = ComputeEdgeNormal(ip, 2, 0);
+		break;
+	}
 	return Output;
 }
